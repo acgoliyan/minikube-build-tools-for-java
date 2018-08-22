7246aeb40cf5c33f50ecb5a305af6e4ef4e4fd96
acgoliyan <aa.c.7172@gmail.com>
2018-08-23 00:10:03 +0530

    Corrected spelling mistake.

--- kokoro/continuous.bat
+++ kokoro/continuous.bat
@@ -3,7 +3,7 @@
 cd github/minikube-build-tools-for-java
 
 rem there is no minikube installed on this system, so any integration test will probably not work
-rem we porbably also have to install VirtualBox or some other VM driver, it not even clear to me
+rem we probably also have to install VirtualBox or some other VM driver, it not even clear to me
 rem that kokoro will allow us to run a VM.
 
 cd minikube-gradle-plugin && call gradlew.bat clean build && ^
